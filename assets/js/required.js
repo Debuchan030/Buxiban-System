bda9575a735cede7c23fb220802fed9043e236ec
Debuchan030 <debuchan030@gmail.com>
2021-08-17 18:50:56 +0800

    bulletin board

---
+++ assets/js/required.js
@@ -0,0 +1,10 @@
+var required = new Vue({
+    el: '#required',
+    template:`
+    <script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+    `,
+})
