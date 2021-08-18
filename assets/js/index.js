7e5387bf402ec8242573ab1d04446e6cbd982358
Debuchan030 <debuchan030@gmail.com>
2021-08-18 17:14:21 +0800

    bulletin board re-design

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,9 @@
 
-
-var edit = document.getElementById('edit');
-console.log(edit)
-edit.addEventListener('click',edit_enable);
-
-// initial
-edit_flag = 0;//0 =  編輯 ， 1 = 儲存
-var title = document.getElementById('title');
-title.disabled= true;
-
-var content = document.getElementById('content');
-content.disabled = true;
+// var post_record = document.getElementById("post_record")
+// var textarea = post_record.querySelectorAll('textarea')
+// for(var i = 0; i < textarea.length; i++){
+//     textarea[i].disabled = "true"
+//     console.log(textarea[i].disabled)
+// }
 
 
-
-
-
-
-
-
-// function
-function edit_enable(){//edit btn
-    if(edit_flag ==0){
-        document.getElementById('title').disabled = false
-        document.getElementById('content').disabled = false
-        document.getElementById('edit').innerHTML = "儲存";
-        edit_flag = 1;
-    }
-    else if(edit_flag == 1){
-        document.getElementById('title').disabled = true
-        document.getElementById('content').disabled = true
-        document.getElementById('edit').innerHTML = "編輯";
-        edit_flag = 0;
-    }
-}
