0dc823c5505f4eaa5143196363e80a916100fb95
tong1226 <tr122688@gmail.com>
2021-08-20 18:32:02 +0800

    已到班_狀態切換

--- assets/js/member_management.js
+++ assets/js/member_management.js
@@ -12,7 +12,8 @@ for (var i = 0; i < parseInt(parent_id) /*parent_id.length*/; i++) {
     var parent_name = i ; /*parent_list[i].parent_name*/
     var parent_acct = i ; /*parent_list[i].parent_acct*/
     var parent_pwd = i ; /*parent_list.parent_pwd*/
-    var parent_phone = i /*parent_list.parent_phone*/;       
-    var std_name = i /*std_list[i].std_name*/;
+    var parent_phone = i ; /*parent_list.parent_phone*/       
+    var std_name = i ; /*std_list[i].std_name*/
     var table_list = "<tr><td>"+std_name+"</td><td>"+parent_name+"</td></tr>" /*列出parent_list[i]*/
 }
+
