b7c42de46b3604f491b03d62ce75c51b0fdd1f6a
tong1226 <tr122688@gmail.com>
2021-08-20 17:56:40 +0800

    學生/家長管理_動態新增表格

---
+++ assets/js/member_management.js
@@ -0,0 +1,18 @@
+//新增學生/家長資料
+//學生列表 std_list[i]
+for (var i = 0; i < parseInt(std_id) /*std_id.length*/; i++) {
+        
+    var std_name = i ; /*std_list[i].std_name*/
+    var parent_name = i ; /*std_list[i].parent_name*/
+    var table_list = "<tr><td>"+std_name+"</td><td>"+parent_name+"</td></tr>" /*列出std_list[i]*/
+}
+
+//家長列表 parent_list[i]
+for (var i = 0; i < parseInt(parent_id) /*parent_id.length*/; i++) {
+    var parent_name = i ; /*parent_list[i].parent_name*/
+    var parent_acct = i ; /*parent_list[i].parent_acct*/
+    var parent_pwd = i ; /*parent_list.parent_pwd*/
+    var parent_phone = i /*parent_list.parent_phone*/;       
+    var std_name = i /*std_list[i].std_name*/;
+    var table_list = "<tr><td>"+std_name+"</td><td>"+parent_name+"</td></tr>" /*列出parent_list[i]*/
+}
