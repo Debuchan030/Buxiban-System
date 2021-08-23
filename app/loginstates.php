be749471543efed02359bd1a740d91f01fe3cd28
Milofa <milofa.work@gmail.com>
2021-08-23 19:07:37 +0800

    (

--- app/loginstates.php
+++ app/loginstates.php
@@ -15,7 +15,7 @@ switch ( $_POST['action'] ) {
             include("dbconfig.php");
             $sqlresult =$conn->query("select * from buxiban_user where user_acct = '$acct' and user_pwd=md5('$pwd')");
             $user = $sqlresult->fetch();
-            print_r $user);
+            print_r($user);
             if($user){
                 $_SESSION['user_id'] = $user['user_id'];
                 $_SESSION['user_name'] = $user['user_name'];
