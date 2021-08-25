a7b9a9ba24c865616ad4f4598516726a03558ad8
Milofa <milofa.work@gmail.com>
2021-08-25 17:33:18 +0800

    get debug

--- app/dbtest.php
+++ app/dbtest.php
@@ -1,4 +1,5 @@
 <?php
+    ini_set("display_errors", "On"); 
     include('dbconfig.php');
     $sqlresult = $conn->query("select * from buxiban_bulletin where buxiban_id= $buxiban_id");
     $bulletin = $sqlresult->fetch();
