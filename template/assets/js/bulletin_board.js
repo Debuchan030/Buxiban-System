8a53d4bd10a0161937020802c483e3379b0077c8
Debuchan030 <debuchan030@gmail.com>
2021-08-25 13:20:21 +0800

    繳款通知尚未完成

--- template/assets/js/bulletin_board.js
+++ template/assets/js/bulletin_board.js
@@ -2,15 +2,17 @@
 var bulletin_template = ({ bulletin_id, bulletin_title, bulletin_content, bulletin_time }) => `
 <tr data-bs-toggle="collapse" data-bs-target="#${bulletin_id}" aria-expanded="false"
 aria-controls="content">
-<td><textarea cols="30" rows="1">${bulletin_title}</textarea></td>
+    <td><textarea cols="30" rows="1">${bulletin_title}</textarea></td>
 </tr>
 <tr class="collapse" id="${bulletin_id}">
-<td><textarea name="" id="" cols="30" rows="3"
-        class="m-2">${bulletin_content}</textarea>
-    <textarea name="" id="" cols="30" rows="1"
-        class="m-2">${bulletin_time}</textarea>
-        <button class="m-2 bulletin_save">儲存</button><button class="m-2 bulletin_delete">刪除</button></td>
-</tr>
+    <td>
+        <textarea name="" id="" cols="30" rows="3"
+            class="m-2">${bulletin_content}</textarea>
+        <textarea name="" id="" cols="30" rows="1"
+            class="m-2">${bulletin_time}</textarea>
+            <button class="m-2 bulletin_save">儲存</button>
+            <button class="m-2 bulletin_delete">刪除</button></td>
+    </tr>
 `;
 
 // 獲取所有公佈欄資訊 id title content time , buxiban_bulletin.length
@@ -29,6 +31,10 @@ for (var i = 0; i < 5 /*buxiban.length*/; i++) {
 // 修改貼文
 $(".bulletin_save").on("click",bulletin_save_func)
 function bulletin_save_func(){
-    console.log()
+    console.log("save")
 }
 //刪除貼文
+$(".bulletin_delete").on("click",bulletin_delete_func)
+function bulletin_delete_func(){
+    console.log("delete")
+}
--- template/assets/js/payment_notice.js
+++ template/assets/js/payment_notice.js
@@ -1,8 +1,100 @@
-var non_payed = document.querySelectorAll('.non_payed')
-// console.log(non_payed)
-for (var i = 0; i < non_payed.length; i++) {
-    non_payed[i].addEventListener('click', non_payed_func)
-}
-function non_payed_func() { //換成已繳費
-    console.log("hi")
+var payment_template = ({ payment_id, std_id, std_name, payment_deadline, total_price, parent_name, selcourse_name, selcourse_price }) => `
+<div class="border border-secondary border-3 rounded p-2 h1 text-center bg-white" data-bs-toggle="collapse"
+			data-bs-target="#${payment_id}" aria-expanded="false" aria-controls="${payment_id}">
+			${payment_deadline}
+		</div>
+
+		<div class="collapse" id="${payment_id}">
+			<div>
+				<table>
+					<h3>未繳款</h3>
+					<tbody id="payed">
+						<!-- <form action="" method="GET"> -->
+							<tr data-bs-toggle="collapse" data-bs-target="#${std_id}" aria-expanded="false"
+							aria-controls="non_payed1">
+							<td>${std_name}</td>
+							<td>繳費期限：${payment_deadline}</td>
+							<td>總費用：$${total_price}</td>
+							<td><button class="non_payed">未繳款</button></td>
+							<td><button>未發送繳款訊息</button></td>
+						</tr>
+						
+						<tr class="collapse" id="${std_id}">
+							<td>
+								<div>
+									姓名：${std_name}
+								</div>
+								<div>
+									家長：${parent_name}
+								</div>
+								<div>
+									詳細資訊：
+								</div>
+								<div>
+								${selcourse_name}--$${selcourse_price}
+								</div>
+								<div>
+								${selcourse_name}--$${selcourse_price}
+								</div>
+								<div>
+									繳費期限：${total_price}
+								</div>
+							</td>
+						</tr>
+					</tbody>
+
+				</table>
+			</div>
+			<!-- <button class="mb-5">一鍵催繳</button> -->
+
+			<div>
+				<table>
+					<h3>已繳款</h3>
+					<tbody id="non_payed">
+						<!-- <form action="" method="GET"> -->
+							<tr data-bs-toggle="collapse" data-bs-target="#${std_id}" aria-expanded="false"
+							aria-controls="non_payed1">
+							<td>${std_name}</td>
+							<td>繳費期限：${payment_deadline}</td>
+							<td>總費用：$${total_price}</td>
+							<td><button class="non_payed">未繳款</button></td>
+							<td><button>未發送繳款訊息</button></td>
+						</tr>
+						
+						<tr class="collapse" id="${std_id}">
+							<td>
+								<div>
+									姓名：${std_name}
+								</div>
+								<div>
+									家長：${parent_name}
+								</div>
+								<div>
+									詳細資訊：
+								</div>
+								<div>
+								${selcourse_name}--$${selcourse_price}
+								</div>
+								<div>
+								${selcourse_name}--$${selcourse_price}
+								</div>
+								<div>
+									繳費期限：${total_price}
+								</div>
+							</td>
+						</tr>
+					</tbody>
+
+				</table>
+			</div>
+		</div>
+`
+
+//獲取學生資料、家長資料、
+
+for (var i = 0; i < 5 /*buxiban_payment.length*/ ; i++) {
+    var payment_state = true //buxiban_payment[i].payment_state true = 有繳款 false = 未繳款
+    if(payment_state){
+
+    }
 }
