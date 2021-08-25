72012bf9a3570ecf065d0e652357b01a705ca7c9
Milofa <milofa.work@gmail.com>
2021-08-25 17:58:30 +0800

    111

--- app/dbtest.php
+++ app/dbtest.php
@@ -2,9 +2,11 @@
     ini_set("display_errors", "On"); 
     include('dbconfig.php');
     $sqlresult = $conn->query("select * from buxiban_bulletin where buxiban_id= 0");
-    print_r("FETCH_ASSOC".$sqlresult->fetch(PDO::FETCH_ASSOC));
-    print_r("FETCH_NUM".$sqlresult->fetch(PDO::FETCH_NUM ));
-    print_r("FETCH_OBJ".$sqlresult->fetch(PDO::FETCH_OBJ ));
-    
+    print("FETCH_ASSOC");
+    print_r($sqlresult->fetch(PDO::FETCH_ASSOC));
+    print("FETCH_NUM");
+    print_r($sqlresult->fetch(PDO::FETCH_NUM ));
+    print("FETCH_OBJ");
+    print_r($sqlresult->fetch(PDO::FETCH_OBJ ));
     
 ?>
