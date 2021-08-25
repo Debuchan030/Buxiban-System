c6eea2f940dfde3e0c66149de3e66f04a3df8fd6
tong1226 <tr122688@gmail.com>
2021-08-25 18:14:34 +0800

    家長動態新增列

--- template/assets/js/member_management.js
+++ template/assets/js/member_management.js
@@ -1,4 +1,3 @@
-//讀取php
 //學生/家長列表 std_list[i]
 $("#std_list").empty()
 for (var i = 0; i < 10 /*std_id.length*/; i++) {
@@ -10,7 +9,6 @@ for (var i = 0; i < 10 /*std_id.length*/; i++) {
     var table_list = "<tr class = \"std_info\"><td>"+std_name+"</td><td>"+parent_name+"</td><td>"+parent_acct+"</td><td>"+parent_pwd+"</td><td>"+parent_phone+"</td><td><button class = \"update_data\">修改資料</button></td><td><button class = \"delete_data\">刪除資料</button></td></tr>"
     $("#std_list").append(table_list)
 }
-
 
 // 刪除名單資料
 $('#std_list').on('click', '.delete_data', delete_data);
@@ -31,7 +29,7 @@ function delete_data(){
 // 修改名單資料
 $('#std_list').on('click', '.update_data', update_data);
 function update_data(){
-    //將std_name、parent_name、parent_acct、parent_pwd、parent_phone刪除
+    //將std_name、parent_name、parent_acct、parent_pwd、parent_phonet修改
     var std_name = $(this).parents(".std_info").find("td").first().text().substring(0)
     var parent_name = $(this).parents(".std_info").find("td").first().next().text().substring(0)
     var parent_acct = $(this).parents(".std_info").find("td").first().next().next().text().substring(0)
@@ -44,6 +42,13 @@ function update_data(){
     console.log(parent_phone)
 }
 
+//動態生成一列
+function add_data_row(){
+    //$("#add_data_write").empty()
+   //var add_list = "<tr><td><input></td><td><input></td><td><input></td></tr>";
+   //$("#add_data_write").append(add_list) 
+   console.log(ha)   
+    }
 
 
 
