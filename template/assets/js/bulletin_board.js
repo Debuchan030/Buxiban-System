92161cf1ee6e0e12d1000a318df012ffdd84722c
Debuchan030 <debuchan030@gmail.com>
2021-08-25 17:08:56 +0800

    bulletin update and delete

--- template/assets/js/bulletin_board.js
+++ template/assets/js/bulletin_board.js
@@ -4,8 +4,8 @@ var bulletin_template = ({ bulletin_id, bulletin_title, bulletin_content, bullet
 aria-controls="content">
     <td><textarea cols="30" rows="1" name = "bulletin_title">${bulletin_title}</textarea></td>
 </tr>
-<tr class="collapse" name = "${bulletin_id}" id="${bulletin_id}">
-    <td>
+<tr class="collapse"  id="${bulletin_id}">
+    <td name = "${bulletin_id}">
         <textarea name="bulletin_content" id="" cols="30" rows="3"
             class="m-2">${bulletin_content}</textarea>
         <textarea name="bulletin_time" id="" cols="30" rows="1"
@@ -37,10 +37,20 @@ $.post("../app/bulletin_board.php",{action: "get_bulletin"}, function(buxiban_bu
 // 修改貼文
 $(".bulletin_save").on("click",bulletin_save_func)
 function bulletin_save_func(){
-    
+    var id = $(this).parent().attr("name")
+    $.post("../app/bulletin_board.php",{action: "update_bulletin",bulletin_id:id}, function(buxiban_bulletin){
+        // Display the returned data in browser
+        // $("#result").html(buxiban_bulletin);
+       
+    });
 }
 //刪除貼文
 $(".bulletin_delete").on("click",bulletin_delete_func)
 function bulletin_delete_func(){
-    console.log("delete")
+    var id = $(this).parent().attr("name")
+    $.post("../app/bulletin_board.php",{action: "delete_bulletin",bulletin_id:id}, function(buxiban_bulletin){
+        // Display the returned data in browser
+        // $("#result").html(buxiban_bulletin);
+       
+    });
 }
