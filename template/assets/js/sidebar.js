d372631e751efb8a42a6af072ee920a30782d27e
Milofa <milofa.work@gmail.com>
2021-08-26 19:44:44 +0800

    fux var name

--- template/assets/js/sidebar.js
+++ template/assets/js/sidebar.js
@@ -7,9 +7,9 @@ $.post("../../app/changepage.php", { action: "get_pagename" }, function (page) {
 */
 
 $('li').click(function () {
-    var targe_tpage = $(this).attr('id');
-    var targe_title = $(this).first().text();
+    var target_tpage = $(this).attr('id');
+    var target_title = $(this).first().text();
     $("#main").load("./template/"+targe_tpage+".html")
-    $("title").html(targe_title)
+    $("title").html(target_title)
     $.post("../../app/changepage.php", {action:"change_page", target_page: targe_tpage,target_title: target_title });
 })
