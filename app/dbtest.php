93b530361e2d4fc192d60cf90507f3d0c813c235
Debuchan030 <debuchan030@gmail.com>
2021-08-26 18:57:42 +0800

    註解dbtest

--- app/dbtest.php
+++ app/dbtest.php
@@ -10,14 +10,13 @@ $buxiban_id = $_SESSION['buxiban_id'];
     echo json_encode($bulletin);
 */
 
-ini_set("display_errors", "On");
-include('dbconfig.php');
-session_start();
-$buxiban_id = $_SESSION['buxiban_id'];
-$back = getenv("HTTP_REFERER");
-$sqlresult = $conn->query("select std_id from buxiban_selcourse where course_id= 1");
-print($sqlresult);
-$std_id_array = $sqlresult->fetchAll(PDO::FETCH_ASSOC);
-print($std_id_array);
+// ini_set("display_errors", "On");
+// include('dbconfig.php');
+// session_start();
+// $buxiban_id = $_SESSION['buxiban_id'];
+// $sqlresult = $conn->query("select std_id from buxiban_selcourse where course_id= 1");
+// print($sqlresult);
+// $std_id_array = $sqlresult->fetchAll(PDO::FETCH_ASSOC);
+// print($std_id_array);
 
 ?>
