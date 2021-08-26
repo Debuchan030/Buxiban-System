ad50b7ef0d874fff30ee467d9728449da94ea410
tong1226 <tr122688@gmail.com>
2021-08-26 17:46:44 +0800

    08/26_學生/家長管理.動態新增多行??

--- app/member_management.php
+++ app/member_management.php
@@ -1,24 +1,27 @@
 <?php
-/*
 ini_set("display_errors", "On"); 
 include('dbconfig.php');
 session_start();
 $buxiban_id = $_SESSION['buxiban_id'];
+$datetime = date("Y-m-d H:i:s");
 $back = getenv("HTTP_REFERER");
-switch ($_POST['action']) { 
-    //取得名單
-    case "get_bulletin":
-        $sqlresult = $conn->query("select * from buxiban_bulletin where buxiban_id= $buxiban_id");
-        $bulletin = $sqlresult->fetchall(PDO::FETCH_OBJ);
-        echo json_encode($bulletin);
+switch ($_POST['action']) {
+    //取得學生/家長名單
+    case "get_list":
+        $sqlresult = $conn->query("select buxiban_student.id,buxiban_student.name,buxiban_parent.name,buxiban_parent.acct,buxiban_parent.pwd,buxiban_parent.phone from buxiban_student inner join buxiban_parent where buxiban_id= $buxiban_id");
+        $member = $sqlresult->fetchall(PDO::FETCH_OBJ);
+        echo json_encode($member);
     break;
-
-    //新增一則公告
-    case "add_bulletin": 
-        //print_r($_POST);
-        $bulletin_title = $_POST['bulletin_title'];
-        $bulletin_content = $_POST['bulletin_content'];
-        $sqlsend = $conn->query("Insert into buxiban_bulletin(bulletin_title,bulletin_content,bulletin_time,buxiban_id) value('$bulletin_title','$bulletin_content','$datetime',$buxiban_id);");
+     //新增學生/家長資料
+    case "add_data": 
+        $std_name = $_POST['std_name'];
+        $parent_name = $_POST['parent_name'];
+        $parent_acct = $_POST['parent_acct'];
+        $parent_pwd = $_POST['parent_pwd'];
+        $parent_phone = $_POST['parent_phone'];
+        $sql = "Insert into buxiban_student(std_name) value('$std_name',$buxiban_id);";
+        $sql .= "Insert into buxiban_parent(parent_name,parent_acct,parent_pwd,parent_phone) value('$parent_name','$parent_acct','$parent_acct','$parent_pwd','$parent_acct',$buxiban_id);";
+        $sqlsend = $conn->multi_query($sql);
         $conn=null;
         if($sqlsend){
             echo '<script> window.alert("新增成功");</script>';
@@ -26,18 +29,23 @@ switch ($_POST['action']) {
         else{
             echo '<script> window.alert("出現錯誤!請聯繫HCT工程部專員");</script>';
         }
-        header("location:$back");
-    break;
-    
-    //更新該則公告
-    case "update_bulletin":
-        $bulletin_id = $_POST['bulletin_id'];
-        $bulletin_title = $_POST['bulletin_title'];
-        $bulletin_content = $_POST['bulletin_content'];
-        $sqlsend = $conn->query("update buxiban_bulletin set bulletin_title='$bulletin_title' ,bulletin_content='$bulletin_content',bulletin_time='$datetime' where bulletin_id = $bulletin_id;");
+            header("location:$back");
+        break;
+
+    //修改學生/家長資料
+    case "update_data":
+        $std_id = $_POST['std_id'];    
+        $std_name = $_POST['std_name'];
+        $parent_name = $_POST['parent_name'];
+        $parent_acct = $_POST['parent_acct'];
+        $parent_pwd = $_POST['parent_pwd'];
+        $parent_phone = $_POST['parent_phone'];
+        $sql = "update buxiban_student set std_name='$std_name' where std_id = $std_id;"
+        $sql .= "update buxiban_parent set parent_name='$parent_name',parent_acct='$parent_acct',parent_pwd='$parent_pwd',parent_phone='$parent_phone' where parent_id = $std_id;"
+        $sqlsend = $conn->multi_query($sql);
         $conn=null;
         if($sqlsend){
-            echo '<script> window.alert("更新成功");</script>';
+            echo '<script> window.alert("修改成功");</script>';
         }
         else{
             echo '<script> window.alert("出現錯誤!請聯繫HCT工程部專員");</script>';
@@ -45,13 +53,15 @@ switch ($_POST['action']) {
         header("location:$back");
     break;
 
-    //刪除該則公告
-    case "delete_bulletin":
-        $bulletin_id = $_POST['bulletin_id'];
-        $sqlsend = $conn->query("delete from buxiban_bulletin where bulletin_id = $bulletin_id;");
+    //刪除學生/家長資料
+    case "delete_data":
+        $std_id = $_POST['std_id'];
+        $sql = "delete from buxiban_student where std_id = $std_id;"
+        $sql .= "delete from buxiban_parent where parent_id = $std_id;"
+        $sqlsend = $conn->query($sql);
         $conn=null;
         if($sqlsend){
-            echo '<script> window.alert("更新成功");</script>';
+            echo '<script> window.alert("刪除成功");</script>';
         }
         else{
             echo '<script> window.alert("出現錯誤!請聯繫HCT工程部專員");</script>';
@@ -59,5 +69,4 @@ switch ($_POST['action']) {
         header("location:$back");
     break;
 }
-
 ?>
--- template/assets/js/member_management.js
+++ template/assets/js/member_management.js
@@ -1,45 +1,56 @@
-//學生/家長列表 std_list[i]
-$("#std_list").empty()
-for (var i = 0; i < 10 /*std_id.length*/; i++) {
-    var std_name = i ; /*std_list[i].std_name*/
-    var parent_name = i+1 ; /*std_list[i].parent_name*/
-    var parent_acct = "HCTxxxxxxx"+i ; /*std_list[i].parent_acct*/
-    var parent_pwd = "HCTxxxxxxx"+i ; /*std_list[i].parent_pwd*/
-    var parent_phone = "09xxxxxx"+i ; /*std_list[i].parent_phone*/
-    var table_list = "<tr class = \"std_info\"><td>"+std_name+"</td><td>"+parent_name+"</td><td>"+parent_acct+"</td><td>"+parent_pwd+"</td><td>"+parent_phone+"</td><td><button class = \"update_data\">修改資料</button></td><td><button class = \"delete_data\">刪除資料</button></td></tr>"
-    $("#std_list").append(table_list)
-}
+// 學生/家長名單模板
+var member_management_template = ({ std_id,std_name, parent_name, parent_acct, parent_pwd, parent_phone }) => `
+<tr id="${std_id}_std_id">
+    <td id="${std_id}_std_name">${std_name}</td>
+    <td id="${std_id}_parent_name">${parent_name}</td>
+    <td id="${std_id}_parent_acct">${parent_acct}</td>
+    <td id="${std_id}_parent_pwd">${parent_pwd}</td>
+    <td id="${std_id}_parent_phone">${parent_phone}</td>
+    <td><button class = "update_data">修改資料</button></td>
+    <td><button class = "delete_data">刪除資料</button></td>
+</tr>
+`;
 
-// 刪除名單資料
-$('#std_list').on('click', '.delete_data', delete_data);
-function delete_data(){
-    //將std_name、parent_name、parent_acct、parent_pwd、parent_phone刪除
-    var std_name = $(this).parents(".std_info").find("td").first().text().substring(0)
-    var parent_name = $(this).parents(".std_info").find("td").first().next().text().substring(0)
-    var parent_acct = $(this).parents(".std_info").find("td").first().next().next().text().substring(0)
-    var parent_pwd = $(this).parents(".std_info").find("td").first().next().next().next().text().substring(0)
-    var parent_phone = $(this).parents(".std_info").find("td").first().next().next().next().next().text().substring(0)
-    console.log(std_name)
-    console.log(parent_name)
-    console.log(parent_acct)
-    console.log(parent_pwd)
-    console.log(parent_phone)
-}
+// 獲取所有學生/家長名單 std_n parent_n aact pwd phone, buxiban_parent.length
+
+$.post("../../app/member_management.php", { action: "get_list" }, function (buxiban_student) {
+
+    buxiban_student = JSON.parse(buxiban_student)
+    for (var i = 0; i < buxiban_student.length; i++) {
+        var id = buxiban_student[i].std_id       
+        var std_n = buxiban_student[i].std_name
+        var parent_n = buxiban_parent[i].parent_name
+        var aact = buxiban_parent[i].parent_acct
+        var pwd = buxiban_parent[i].parent_pwd
+        var phone = buxiban_parent[i].parent_phone
+        $('#std_list').append([
+            { std_id: id,std_name: std_n, parent_name: parent_n, parent_acct: aact, parent_pwd: pwd, parent_phone: phone },
+        ].map(member_management_template));
+    }
+});
 
 // 修改名單資料
-$('#std_list').on('click', '.update_data', update_data);
-function update_data(){
-    //將std_name、parent_name、parent_acct、parent_pwd、parent_phonet修改
-    var std_name = $(this).parents(".std_info").find("td").first().text().substring(0)
-    var parent_name = $(this).parents(".std_info").find("td").first().next().text().substring(0)
-    var parent_acct = $(this).parents(".std_info").find("td").first().next().next().text().substring(0)
-    var parent_pwd = $(this).parents(".std_info").find("td").first().next().next().next().text().substring(0)
-    var parent_phone = $(this).parents(".std_info").find("td").first().next().next().next().next().text().substring(0)
-    console.log(std_name)
-    console.log(parent_name)
-    console.log(parent_acct)
-    console.log(parent_pwd)
-    console.log(parent_phone)
+$("#std_list").on("click", ".update_data", update_data_func)
+function update_data_func() {
+    var id = $(this).parent().attr("name")
+    var std_n_addr = "#" + id + "std_name"
+    var parent_n_addr = "#" + id + "parent_name"
+    var aact_addr = "#" + id + "parent_acct"
+    var pwd_addr = "#" + id + "parent_pwd"
+    var phone_addr = "#" + id + "parent_phone"
+    var std_n = $(std_n_addr).val()
+    var parent_n = $(parent_n_addr).val()
+    var aact = $(aact_addr).val()
+    var pwd = $(pwd_addr).val()
+    var phone = $(phone_addr).val()
+    $.post("../../app/member_management.php", { action: "update_data", std_id: id,std_name: std_n, parent_name: parent_n, parent_acct: aact, parent_pwd: pwd, parent_phone: phone });
+}
+
+//刪除名單資料
+$("#std_list").on("click", ".delete_data", delete_data_func)
+function delete_data_func() {
+    var id = $(this).parent().attr("name")
+    $.post("../../app/member_management.php", { action: "delete_data", std_id: id });
 }
 
 //動態生成一列
