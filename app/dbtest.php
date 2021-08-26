667022a9e31642e1f452c354a43efdd2bb7f5c8b
Milofa <milofa.work@gmail.com>
2021-08-26 19:35:49 +0800

    .

--- app/dbtest.php
+++ app/dbtest.php
@@ -1,8 +1,9 @@
 <?php
+ini_set("display_errors", "On"); 
 /*
 session_start();
 $buxiban_id = $_SESSION['buxiban_id'];
-    ini_set("display_errors", "On"); 
+    
     include('dbconfig.php');
     $sqlresult = $conn->query("select * from buxiban_bulletin where buxiban_id= $buxiban_id");
     $bulletin = $sqlresult->fetchall(PDO::FETCH_OBJ);
