1209fb500dd8c58535de345faa9bc0a6a2ca6943
Milofa <milofa.work@gmail.com>
2021-08-26 17:16:22 +0800

    123

--- template/assets/js/sidebar.js
+++ template/assets/js/sidebar.js
@@ -1,12 +1,14 @@
+/*
 $.post("../../app/changepage.php", { action: "get_pagename" }, function (page) {
     page = JSON.parse(page);
     $("#main").load("./template/"+page[0]+".html")
     $("title").html(page[1])
 });
+*/
 
 $('li').click(function () {
     var targe_tpage = $(this).attr('id');
-    var targe_title =  $(this).first().text();
+    var targe_title = $(this).first().text();
     $("#main").load("./template/"+targe_tpage+".html")
     $("title").html(targe_title)
     $.post("../../app/changepage.php", {action:"change_page", target_page: targe_tpage,target_title: target_title });
