112ccc7914a571fa0a6919dd0aaa102e501f07ce
Milofa <milofa.work@gmail.com>
2021-08-27 22:34:55 +0800

    final

--- app/member_management.php
+++ app/member_management.php
@@ -60,10 +60,12 @@ switch ($_POST['action']) {
     //刪除學生/家長資料
     case "delete_member":
         $std_id = $_POST['std_id'];
+        
         $temp = $conn->query("select parent_id from buxiban_student where std_id = $std_id;")->fetch(PDO::FETCH_ASSOC);
+        //殺小孩
         $sqlresult = $conn->query("delete from buxiban_student where std_id = $std_id;");
         //選課拔除
-        //
+        //查找小孩
         $sqlresult = $conn->query("select * from buxiban_student where parent_id = $temp;")->fetch(PDO::FETCH_ASSOC);
         //沒小孩就砍了家長
         if(!$sqlresult){
