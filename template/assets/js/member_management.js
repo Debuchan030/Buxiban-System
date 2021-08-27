27610b69dae3174517c7c3c0367f977854e702b9
Debuchan030 <debuchan030@gmail.com>
2021-08-27 17:10:50 +0800

    test

--- template/assets/js/member_management.js
+++ template/assets/js/member_management.js
@@ -55,9 +55,7 @@ function update_member_func() {
 
     member_update_list = JSON.stringify(member_update_list)
     if (flag == 1) {
-        console.log("push")
         $.post("../../app/member_management.php", { action: "update_member", member_update_list });
-
     }
     else {
         alert("請勿空格！")
