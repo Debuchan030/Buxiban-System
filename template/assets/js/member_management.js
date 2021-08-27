16601318f6ee7a257c4f1bed9079e84f1fcea674
Debuchan030 <debuchan030@gmail.com>
2021-08-27 20:51:21 +0800

    final?

--- template/assets/js/member_management.js
+++ template/assets/js/member_management.js
@@ -92,7 +92,6 @@ $('#add_data_write').on('click', ".delete_new_member", function () {
 
 //搜尋
 $('#search_text').addEventListener("keydown", event => {
-    console.log(event.keyCode)
     if (event.keyCode === 13) {
         var search_text = $('#search_text').text()
         var count = 0;
@@ -116,10 +115,7 @@ $('#search_text').addEventListener("keydown", event => {
                         ].map(member_management_template));
                         count++
                     }
-
                 }
-
-
             });
             if (count == 0) {
                 alert("查無結果")
