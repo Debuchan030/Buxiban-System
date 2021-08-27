b1120632cefe2ed2677dc2941ab655a090460a76
Debuchan030 <debuchan030@gmail.com>
2021-08-27 20:21:19 +0800

    selcourse

--- app/class_selected_management.php
+++ app/class_selected_management.php
@@ -16,20 +16,30 @@ switch ($_POST['action']) {
     case "get_selcourse_std":
         $course_id = $_POST['course_id'];
         $sqlresult = $conn->query("select std_id from buxiban_selcourse where course_id= $course_id");
-        $std_id_array = $sqlresult->fetchAll(PDO::FETCH_ASSOC);
-        $std_info = $conn->query("select * from buxiban_student where std_id IN($std_id_array)");
-        $std_info = $std_info->fetchAll(PDO::FETCH_OBJ);
-        echo json_encode($std_info);
+        if ($sqlresult) {
+            $std_id_array = $sqlresult->fetchAll(PDO::FETCH_ASSOC);
+            $std_info = $conn->query("select * from buxiban_student where std_id IN($std_id_array)");
+            if ($std_info) {
+                $std_info = $std_info->fetchAll(PDO::FETCH_OBJ);
+                echo json_encode($std_info);
+            }
+        }
+
         break;
 
         //取得未選課之學生
     case "get_nonselcourse_std":
         $course_id = $_POST['course_id'];
         $sqlresult = $conn->query("select std_id from buxiban_selcourse where course_id= $course_id");
-        $std_id_array = $sqlresult->fetchAll(PDO::FETCH_ASSOC);
-        $std_info = $conn->query("select * from buxiban_student where std_id NOT IN($std_id_array)");
-        $std_info = $std_info->fetchAll(PDO::FETCH_OBJ);
-        echo json_encode($std_info);
+        if ($sqlresult) {
+            $std_id_array = $sqlresult->fetchAll(PDO::FETCH_ASSOC);
+            $std_info = $conn->query("select * from buxiban_student where std_id NOT IN($std_id_array)");
+            if ($std_info) {
+                $std_info = $std_info->fetchAll(PDO::FETCH_OBJ);
+                echo json_encode($std_info);
+            }
+        }
+
         break;
 
         //新增選課學生
@@ -38,8 +48,8 @@ switch ($_POST['action']) {
         $std_id = json_decode($_POST['one_btn_add_std_array']);
         foreach ($std_id as &$value) {
             $sqlsend = $conn->query("Insert into buxiban_selcourse(course_id,std_id) value('$course_id','$value');");
-            $conn=null;
-        }   
+            $conn = null;
+        }
         break;
         //刪除選課學生
     case "delete_selcourse":
@@ -48,7 +58,7 @@ switch ($_POST['action']) {
         $std_id = json_decode($_POST['one_btn_delete_std_array']);
         foreach ($std_id as &$value) {
             $sqlsend = $conn->query("delete from buxiban_selcourse where course_id = $course_id AND std_id = $value");
-            $conn=null;
-        }   
+            $conn = null;
+        }
         break;
 }
