d31a863d2316c7ca8185ef63e0f03f233eb4eadb
Debuchan030 <debuchan030@gmail.com>
2021-08-30 16:57:39 +0800

    test

--- template/assets/js/attend_change.js
+++ template/assets/js/attend_change.js
@@ -33,50 +33,53 @@ Date.prototype.yyyymmdd = function () {
     ].join('');
 };
 $(document).ready(function () {
-    var bindDatePicker = function () {
-        $(".date").datetimepicker({
-            format: 'YYYY-MM-DD',
-            icons: {
-                time: "fa fa-clock-o",
-                date: "fa fa-calendar",
-                up: "fa fa-arrow-up",
-                down: "fa fa-arrow-down"
-            }
-        }).find('input:first').on("blur", function () {
-            // check if the date is correct. We can accept dd-mm-yyyy and yyyy-mm-dd.
-            // update the format if it's yyyy-mm-dd
-            var date = parseDate($(this).val());
+    $(function () {
+        var bindDatePicker = function () {
+            $(".date").datetimepicker({
+                format: 'YYYY-MM-DD',
+                icons: {
+                    time: "fa fa-clock-o",
+                    date: "fa fa-calendar",
+                    up: "fa fa-arrow-up",
+                    down: "fa fa-arrow-down"
+                }
+            }).find('input:first').on("blur", function () {
+                // check if the date is correct. We can accept dd-mm-yyyy and yyyy-mm-dd.
+                // update the format if it's yyyy-mm-dd
+                var date = parseDate($(this).val());
 
-            if (!isValidDate(date)) {
-                //create date based on momentjs (we have that)
-                date = moment().format('YYYY-MM-DD');
-            }
+                if (!isValidDate(date)) {
+                    //create date based on momentjs (we have that)
+                    date = moment().format('YYYY-MM-DD');
+                }
 
-            $(this).val(date);
-        });
-    }
-
-    var isValidDate = function (value, format) {
-        format = format || false;
-        // lets parse the date to the best of our knowledge
-        if (format) {
-            value = parseDate(value);
+                $(this).val(date);
+            });
         }
 
-        var timestamp = Date.parse(value);
+        var isValidDate = function (value, format) {
+            format = format || false;
+            // lets parse the date to the best of our knowledge
+            if (format) {
+                value = parseDate(value);
+            }
 
-        return isNaN(timestamp) == false;
-    }
+            var timestamp = Date.parse(value);
 
-    var parseDate = function (value) {
-        var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
-        if (m)
-            value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
+            return isNaN(timestamp) == false;
+        }
 
-        return value;
-    }
+        var parseDate = function (value) {
+            var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
+            if (m)
+                value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
 
-    bindDatePicker();
+            return value;
+        }
+
+        bindDatePicker();
+    });
+
 });
 
 
