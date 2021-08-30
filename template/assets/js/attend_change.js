45da9210c466c6fa06cbc4e9507d7c72569750e8
Debuchan030 <debuchan030@gmail.com>
2021-08-30 16:41:17 +0800

    test

--- template/assets/js/attend_change.js
+++ template/assets/js/attend_change.js
@@ -1,11 +1,15 @@
 //獲取php資料
 
-var attend_template = ({ std_id, std_name, parent_name, parent_phone, remark }) => `
+var attend_template = ({ std_id, std_name, parent_name, parent_phone, attend_time, leave_time, remark }) => `
 
 <tr id="${std_id}_attend">
     <td>${std_name}</td>
     <td>${parent_name}</td>
-    <td>${parent_phone}</td>>
+    <td>${parent_phone}</td>
+    <td>
+        <label for="">到班時間：${attend_time}</label>
+        <label for="">離班時間：${leave_time}</label>
+    </td>
     <td>
         <input type="radio" name="${std_id}_attend_state" id="${std_id}_not_attend" value="未到班">
         <input type="radio" name="${std_id}_attend_state" id="${std_id}_attended" value="到班">
@@ -18,9 +22,25 @@ var attend_template = ({ std_id, std_name, parent_name, parent_phone, remark }) 
 </tr>
 
 `
+
+Date.prototype.yyyymmdd = function () {
+    var mm = this.getMonth() + 1; // getMonth() is zero-based
+    var dd = this.getDate();
+
+    return [this.getFullYear(),
+    (mm > 9 ? '' : '0') + mm,
+    (dd > 9 ? '' : '0') + dd
+    ].join('');
+};
+
+$('.datepicker').datepicker({
+    format: 'yyyy/dd/mm',
+    // startDate: '-3d'
+});
+
 //建立所有學生列表
-function get_attend_table() {
-    $.post("../../app/attend_record.php", { action: "get_attend" }, function (attend) {
+function get_attend_table(date) {
+    $.post("../../app/attend_record.php", { action: "get_attend", date: date }, function (attend) {
 
         attend = JSON.parse(attend)
         for (var i = 0; i < attend.length; i++) {
@@ -28,11 +48,12 @@ function get_attend_table() {
             var std_n = attend[i].std_name
             var parent_n = attend[i].parent_name
             //確認狀態 0 未到班 1 到班 2 離班 3 請假
-
+            var attend_time = attend[i].attend_time
+            var leave_time = attend[i].leave_time
             var phone = attend[i].parent_phone
             var remark = attend[i].remark
             $('#attend_student').append([
-                { std_id: id, std_name: std_n, parent_name: parent_n, parent_phone: phone, remark },
+                { std_id: id, std_name: std_n, parent_name: parent_n, parent_phone: phone, attend_time: attend_time, leave_time: leave_time, remark: remark },
             ].map(attend_template));
 
             var attend_states = attend[i].attend_states
@@ -51,7 +72,9 @@ function get_attend_table() {
         }
     });
 }
-get_attend_table.call()
+var today = new Date();
+
+get_attend_table.call(today.yyyymmdd())
 
 // 修改狀態
 $('#attend_student').on('click', $('input:radio'), update_attend_table)
@@ -83,7 +106,53 @@ $('#attend_student').on('keydown', $('input:text'), event => {
         $.post("../../app/attend_record.php", { action: "update_remark", student_id: id, remark: remark })
     }
 })
+//搜尋
+$('#search_text').on("keydown", event => {
+    if (event.keyCode === 13) {
+        var search_text = $('#search_text').val()
+        if (search_text == "") {
+            $('#attend_student').empty()
+            get_attend_table.call()
+        }
+        else {
+            $('#attend_student').empty()
 
+            $.post("../../app/attend_record.php", { action: "get_attend" }, function (attend) {
+
+                attend = JSON.parse(attend)
+                for (var i = 0; i < attend.length; i++) {
+                    var id = attend[i].std_id
+                    var std_n = attend[i].std_name
+                    var parent_n = attend[i].parent_name
+                    //確認狀態 0 未到班 1 到班 2 離班 3 請假
+
+                    var phone = attend[i].parent_phone
+                    var remark = attend[i].remark
+                    if (std_n.indexOf(search_text) != -1 || parent_n.indexOf(search_text) != -1 || phone.indexOf(search_text) != -1 || remark.indexOf(search_text) != -1) {
+                        $('#attend_student').append([
+                            { std_id: id, std_name: std_n, parent_name: parent_n, parent_phone: phone, remark },
+                        ].map(attend_template));
+
+                        var attend_states = attend[i].attend_states
+                        if (attend_states == 0) {
+                            $("#" + id + "_not_attend").prop('checked', true);
+                        }
+                        else if (attend_states == 1) {
+                            $("#" + id + "_attended").prop('checked', true);
+                        }
+                        else if (attend_states == 2) {
+                            $("#" + id + "_leaved").prop('checked', true);
+                        }
+                        else {
+                            $("#" + id + "_day_off").prop('checked', true);
+                        }
+                    }
+                }
+                alert("以下為搜尋結果")
+            });
+        }
+    }
+})
 
 
 
