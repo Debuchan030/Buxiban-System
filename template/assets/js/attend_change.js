9d990cc8982aba76af47d4a1e528cd9d8cfb7a97
Debuchan030 <debuchan030@gmail.com>
2021-08-30 15:17:39 +0800

    到班通知管理前端部分

--- template/assets/js/attend_change.js
+++ template/assets/js/attend_change.js
@@ -1,24 +1,89 @@
 //獲取php資料
-//建立所有學生列表
 
-var attend_template = ({ }) => `
+var attend_template = ({ std_id, std_name, parent_name, parent_phone, remark }) => `
 
 <tr id="${std_id}_attend">
     <td>${std_name}</td>
     <td>${parent_name}</td>
     <td>${parent_phone}</td>>
     <td>
-        <input type="radio" name="attend_state" id="" value="未到班">
-        <input type="radio" name="attend_state" id="" value="到班">
-        <input type="radio" name="attend_state" id="" value="離班">
-        <input type="radio" name="attend_state" id="" value="請假">
+        <input type="radio" name="${std_id}_attend_state" id="${std_id}_not_attend" value="未到班">
+        <input type="radio" name="${std_id}_attend_state" id="${std_id}_attended" value="到班">
+        <input type="radio" name="${std_id}_attend_state" id="${std_id}_leaved" value="離班">
+        <input type="radio" name="${std_id}_attend_state" id="${std_id}_day_off" value="請假">
     </td>
     <td>
-
+        <input id="${std_id}_remark" type="text" value="${remark}" >
     </td>
 </tr>
 
 `
+//建立所有學生列表
+function get_attend_table() {
+    $.post("../../app/attend_record.php", { action: "get_attend" }, function (attend) {
+
+        attend = JSON.parse(attend)
+        for (var i = 0; i < attend.length; i++) {
+            var id = attend[i].std_id
+            var std_n = attend[i].std_name
+            var parent_n = attend[i].parent_name
+            //確認狀態 0 未到班 1 到班 2 離班 3 請假
+
+            var phone = attend[i].parent_phone
+            var remark = attend[i].remark
+            $('#attend_student').append([
+                { std_id: id, std_name: std_n, parent_name: parent_n, parent_phone: phone, remark },
+            ].map(attend_template));
+
+            var attend_states = attend[i].attend_states
+            if (attend_states == 0) {
+                $("#" + id + "_not_attend").prop('checked', true);
+            }
+            else if (attend_states == 1) {
+                $("#" + id + "_attended").prop('checked', true);
+            }
+            else if (attend_states == 2) {
+                $("#" + id + "_leaved").prop('checked', true);
+            }
+            else {
+                $("#" + id + "_day_off").prop('checked', true);
+            }
+        }
+    });
+}
+get_attend_table.call()
+
+// 修改狀態
+$('#attend_student').on('click', $('input:radio'), update_attend_table)
+function update_attend_table() {
+    var id = $(this).attr('name')
+    id = id.substring(0, id.length - 13)
+    var attend_states = 0
+    if ($(this).attr('value') == "未到班") {
+        attend_states = 0
+    }
+    else if ($(this).attr('value') == "到班") {
+        attend_states = 1
+    }
+    else if ($(this).attr('value') == "離班") {
+        attend_states = 2
+    }
+    else {
+        attend_states = 3
+    }
+    $.post("../../app/attend_record.php", { action: "update_attend_states", student_id: id, attend_states: attend_states })
+}
+
+// 修改備註
+$('#attend_student').on('keydown', $('input:text'), event => {
+    if (event.keyCode === 13) {
+        var id = $(this).attr('id')
+        id = id.substring(0, id.length - 7)
+        var remark = $(this).val()
+        $.post("../../app/attend_record.php", { action: "update_remark", student_id: id, remark: remark })
+    }
+})
+
 
 
 
