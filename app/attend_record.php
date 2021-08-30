5f96609cbff8991456e1531e8483f42587b026b5
Milofa <milofa.work@gmail.com>
2021-08-30 17:28:31 +0800

    rename

--- app/attend_record.php
+++ app/attend_record.php
@@ -8,8 +8,8 @@ switch ($_POST['action']) {
     //取得學生/家長名單
     case "get_member":
         $sqlresult = $conn->query("select buxiban_student.std_name,buxiban_parent.parent_name,buxiban_parent.parent_phone,buxiban_attend.attend_time,buxiban_attend.leave_time,buxiban_attend.attend_states,buxiban_attend.remark FROM buxiban_student,buxiban_parent,buxiban_attend WHERE buxiban_student.parent_id=buxiban_parent.parent_id AND buxiban_attend.std_id = buxiban_student.std_id AND buxiban_student.buxiban_id = $buxiban_id");
-        $member = $sqlresult->fetchall(PDO::FETCH_ASSOC);
-        echo json_encode($member);
+        $atten = $sqlresult->fetchall(PDO::FETCH_ASSOC);
+        echo json_encode($atten);
     break;
     /*
     //新增學生/家長資料
