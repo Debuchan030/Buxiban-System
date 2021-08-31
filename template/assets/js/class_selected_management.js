3e3190938994cd68873ca7f1c8a3cdf789360f68
Debuchan030 <debuchan030@gmail.com>
2021-08-31 17:18:39 +0800

    return

--- template/assets/js/class_selected_management.js
+++ template/assets/js/class_selected_management.js
@@ -33,6 +33,9 @@ function change_student_list() { //依據選擇的課程來分類有選課/未�
         one_btn_delete_std_array = []
         //建立已選修學生之列表
         $.post("../../app/class_selected_management.php", { action: "get_selcourse_std", course_id: selected_course_id }, function (std) {
+            if(std == "no data"){
+                return
+            }
             std = JSON.parse(std)
 
             for (var i = 0; i < std.length; i++) {
@@ -44,6 +47,9 @@ function change_student_list() { //依據選擇的課程來分類有選課/未�
         });
         //建立未選修學生之列表
         $.post("../../app/class_selected_management.php", { action: "get_nonselcourse_std", course_id: selected_course_id }, function (std) {
+            if(std == "no data"){
+                return
+            }
             std = JSON.parse(std)
 
             for (var i = 0; i < std.length; i++) {
