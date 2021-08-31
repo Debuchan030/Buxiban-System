d28eb506d03b8fe54627689d17691c643b36af72
Milofa <milofa.work@gmail.com>
2021-08-31 18:25:20 +0800

    debug

--- template/assets/js/sidebar.js
+++ template/assets/js/sidebar.js
@@ -8,7 +8,9 @@ $.post("../../app/changepage.php", { action: "get_pagename" }, function (page) {
 $('li').click(function () {
     var target_page = $(this).attr('id');
     var target_title = $(this).first().text();
-    $("#main").load("./template/"+target_page+".html")
-    $("title").html(target_title)
-    $.post("../../app/changepage.php", {action:"change_page", target_page: target_page,target_title: target_title });
+    if(target_page){
+        $("#main").load("./template/"+target_page+".html")
+        $("title").html(target_title)
+        $.post("../../app/changepage.php", {action:"change_page", target_page: target_page,target_title: target_title });
+    }
 })
