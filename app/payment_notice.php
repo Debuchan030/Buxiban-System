a4d81b01757496835d988be5db6cfc9baf662cf5
Milofa <milofa.work@gmail.com>
2021-09-01 17:34:12 +0800

    fix buxiban name

--- app/payment_notice.php
+++ app/payment_notice.php
@@ -44,7 +44,7 @@ switch ($_POST['action']) {
             echo("get");
         }
         else{
-            $add_new_payment = $conn->query("insert into buxiban_payment(buxiban_id,payment_time) value($buxiaban_id,'$datemonth')");
+            $add_new_payment = $conn->query("insert into buxiban_payment(buxiban_id,payment_time) value($buxiban_id,'$datemonth')");
             echo("non get");
             $get_std_list = $conn->query("select std_id,std_name,buxiban_parent.parent_id,parent_name,parent_phone from buxiban_student,buxiban_parent where buxiban_student.parent_id = buxiban_parent.parent_id AND buxiban_student.buxiban_id=$buxiban_id")->fetchAll(PDO::FETCH_ASSOC);
             for($i=0;$i < count($get_std_list);$i++){
