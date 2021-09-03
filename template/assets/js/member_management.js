2ca345642fb2d61b6276f8fd853f942a768da11b
Debuchan030 <debuchan030@gmail.com>
2021-09-03 14:39:42 +0800

    delete

--- template/assets/js/member_management.js
+++ template/assets/js/member_management.js
@@ -93,7 +93,7 @@ function update_member_func() {
 //刪除名單資料
 $("#member_info").on("click", ".delete_member", delete_member_func)
 function delete_member_func() {
-    var id = $(this).parent().parent().attr("id")
+    var id = $(this).closest("tr").attr("id")
     id = id.substring(0, id.length - 7)
     $.post("../../app/member_management.php", { action: "delete_member", std_id: id });
     location.reload();
